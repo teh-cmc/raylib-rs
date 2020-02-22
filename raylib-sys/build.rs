e2c9b9fd0bf30c04fff199f690a5732a91b77f90
Clement Rey <cr.rey.clement@gmail.com>
2020-02-22 19:34:18 +0100

    raylib-sys: linking with raylib 2.6.0

--- raylib-sys/build.rs
+++ raylib-sys/build.rs
@@ -19,7 +19,7 @@ use std::path::{Path, PathBuf};
 use std::{env, fs};
 
 /// latest version on github's release page as of time or writing
-const LATEST_RAYLIB_VERSION: &str = "2.5.0";
+const LATEST_RAYLIB_VERSION: &str = "2.6.0";
 const LATEST_RAYLIB_API_VERSION: &str = "2";
 
 #[cfg(feature = "nobuild")]
